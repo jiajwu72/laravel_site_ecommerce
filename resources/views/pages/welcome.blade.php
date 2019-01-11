ec920720436ddfb85791d8c9d62b38294c3c76b5
Jiajun WU <jwu@e1r4p8.42.fr>
2019-01-11 14:47:52 +0100

    added

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -15,7 +15,7 @@
             <div class="jumbotron">
                 <h1>High Thechnologie</h1>
                 <p class="lead">Découcrir les meilleurs high tech produits</p>
-                <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
+                <!--<p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>-->
             </div>
         </div>
         <!--end of header .tow-->
@@ -24,7 +24,7 @@
         <div class="col-md-8">
             @foreach ($posts as $post)
             <div class="post">
-                <h3>{{$post->title}}</h3>
+                <a href="{{ route('posts.show', $post->id) }}"><h3>{{$post->title}}</h3></a> 
                 <p>{{ substr($post->body, 0, 50) }} {{strlen($post->body) > 50 ? "..." : ""}}</p>
                 
             </div>
@@ -32,9 +32,10 @@
             <hr>
         </div>
 
+<!--
         <div class="col-md-3 col-md-offset-1" ">
             <h2>Promotion</h2>
         </div>
-        
+!-->
     </div>
 @endsection
